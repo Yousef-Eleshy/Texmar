31625147082d171a590956f80edfacd181f60076
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-03 13:29:41 +0000

    Payment Method

--- payment_method/models/.ipynb_checkpoints/models-checkpoint.py
+++ payment_method/models/.ipynb_checkpoints/models-checkpoint.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class payment_method(models.Model):
     _inherit = 'account.journal'
     
-    bank_account_number = fields.text(string='Account Number', readonly=False)
+    bank_account_number = fields.Char(string='Account Number', readonly=False)
