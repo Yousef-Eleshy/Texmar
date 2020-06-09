607081ee699e1dddfca81a458db5361e28108c7a
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-09 10:29:06 +0000

    Payment Method Fields Added

--- payment_method/models/.ipynb_checkpoints/models-checkpoint.py
+++ payment_method/models/.ipynb_checkpoints/models-checkpoint.py
@@ -13,4 +13,3 @@ class payment_method(models.Model):
     out_bank_account_number = fields.Char(string='Bank Account Number', readonly=False)
     
     out_visa_account_number = fields.Char(string='Visa Account Number', readonly=False)
-
