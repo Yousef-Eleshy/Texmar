bcc8499ad87b060b7b18e82e1df384982e05cead
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-16 09:38:04 +0000

    Changed Field Location

--- payment_method/models/.ipynb_checkpoints/models-checkpoint.py
+++ payment_method/models/.ipynb_checkpoints/models-checkpoint.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 
 class payment_method(models.Model):
-    _inherit = 'account.journal'
+    _inherit = 'account.payment'
     
     #Incoming Payment: Bank Account Number
     in_bank_account_number = fields.Char(string='Bank Account Number', readonly=False)
@@ -16,40 +16,40 @@ class payment_method(models.Model):
     out_visa_account_number = fields.Char(string='Visa Account Number', readonly=False)
     
     # Show Incoming Bank Account Number Field IF bank is selected
-    @api.depends('inbound_payment_method_ids')
-    def _compute_in_bank_payment_method_selected(self):
-        for journal in self:
-            journal.in_bank_payment_method_selected = any(pm.code == 'bank' for pm in journal.inbound_payment_method_ids)
+#    @api.depends('inbound_payment_method_ids')
+#    def _compute_in_bank_payment_method_selected(self):
+#        for journal in self:
+#            journal.in_bank_payment_method_selected = any(pm.code == 'bank' for pm in journal.inbound_payment_method_ids)
             
     
-    in_bank_payment_method_selected = fields.Boolean(compute='_compute_in_bank_payment_method_selected')
+#    in_bank_payment_method_selected = fields.Boolean(compute='_compute_in_bank_payment_method_selected')
     
     
     # Show Incoming Visa Account Number Field IF visa is selected
-    @api.depends('inbound_payment_method_ids')
-    def _compute_in_visa_payment_method_selected(self):
-        for journal in self:
-            journal.in_visa_payment_method_selected = any(pm.code == 'visa' for pm in journal.inbound_payment_method_ids)
+#    @api.depends('inbound_payment_method_ids')
+#    def _compute_in_visa_payment_method_selected(self):
+#        for journal in self:
+#            journal.in_visa_payment_method_selected = any(pm.code == 'visa' for pm in journal.inbound_payment_method_ids)
             
     
-    in_visa_payment_method_selected = fields.Boolean(compute='_compute_in_visa_payment_method_selected')    
+#    in_visa_payment_method_selected = fields.Boolean(compute='_compute_in_visa_payment_method_selected')    
 
     
     
     # Show Outgoing Bank Account Number Field IF bank is selected
-    @api.depends('outbound_payment_method_ids')
-    def _compute_out_bank_payment_method_selected(self):
-        for journal in self:
-            journal.out_bank_payment_method_selected = any(pm.code == 'bank' for pm in journal.outbound_payment_method_ids)
+#    @api.depends('outbound_payment_method_ids')
+#    def _compute_out_bank_payment_method_selected(self):
+#        for journal in self:
+#            journal.out_bank_payment_method_selected = any(pm.code == 'bank' for pm in journal.outbound_payment_method_ids)
             
     
-    out_bank_payment_method_selected = fields.Boolean(compute='_compute_out_bank_payment_method_selected')
+#    out_bank_payment_method_selected = fields.Boolean(compute='_compute_out_bank_payment_method_selected')
     
     # Show Outgoing Visa Account Number Field IF visa is selected
-    @api.depends('outbound_payment_method_ids')
-    def _compute_out_visa_payment_method_selected(self):
-        for journal in self:
-            journal.out_visa_payment_method_selected = any(pm.code == 'visa' for pm in journal.outbound_payment_method_ids)
+#    @api.depends('outbound_payment_method_ids')
+#    def _compute_out_visa_payment_method_selected(self):
+#        for journal in self:
+#            journal.out_visa_payment_method_selected = any(pm.code == 'visa' for pm in journal.outbound_payment_method_ids)
             
     
-    out_visa_payment_method_selected = fields.Boolean(compute='_compute_out_visa_payment_method_selected')    
+#    out_visa_payment_method_selected = fields.Boolean(compute='_compute_out_visa_payment_method_selected')    
